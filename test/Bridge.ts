a449ffbe08ee3dc2267217d7a48372fe8953eab0
Barry Gitarts <bgitarts@gmail.com>
2021-02-10 12:05:13 -0500

    add avalanche bridge and handler to test

--- test/Bridge.ts
+++ test/Bridge.ts
@@ -22,10 +22,13 @@ describe("Bridge", function () {
   const AVA_CHAIN_ID: number = 2;
   const relayerThreshold: number = 2;
   let accounts: Signer[];
-  let bridge: Bridge;
-  let erc20Handler: ERC20Handler;
+  let EthereumBridge: Bridge;
+  let AvalancheBridge: Bridge;
+  let ethereumErc20Handler: ERC20Handler;
   let sntEthereum: MintableERC20;
   let sntEthereumResourceId: string;
+  let sntAvalanche: MintableERC20;
+  let sntAvalancheResourceId: string;
   let deployerAddress: string;
   let depositerAddress: string;
   let recipientAddress: string;
@@ -34,37 +37,49 @@ describe("Bridge", function () {
     accounts = await ethers.getSigners();
   });
 
-  it("should deploy bridge contract", async function () {
+  it("should deploy bridge contracts", async function () {
     const [deployer, depositer, recipient, relayer1, relayer2] = accounts;
     const initialRelayers = [await relayer1.getAddress(), await relayer2.getAddress()];
     deployerAddress = await accounts[0].getAddress();
     const bridgeFactory = await ethers.getContractFactory("Bridge");
-    const _bridge = await bridgeFactory.deploy(
+    const _EthereumBridge = await bridgeFactory.deploy(
       ETHEREUM_CHAIN_ID,
       initialRelayers,
       relayerThreshold,
       0,
       100
     ) as Bridge;
-    expect(_bridge).to.have.property('deployTransaction');
-    bridge = _bridge;
+    const _AvalancheBridge = await bridgeFactory.deploy(
+      AVA_CHAIN_ID,
+      initialRelayers,
+      relayerThreshold,
+      0,
+      100
+    ) as Bridge;
+    expect(_EthereumBridge).to.have.property('deployTransaction');
+    expect(_AvalancheBridge).to.have.property('deployTransaction');
+    EthereumBridge = _EthereumBridge;
+    AvalancheBridge = _AvalancheBridge;
   });
 
   it("Should create erc20 handler", async function () {
     const erc20Factory = await ethers.getContractFactory("MintableERC20");
     const _snt = await erc20Factory.deploy("SNT", "SNT") as MintableERC20;
+    const _sntAvalanche = await erc20Factory.deploy("aSNT", "aSNT") as MintableERC20;
     sntEthereum = _snt;
+    sntAvalanche = _sntAvalanche;
     sntEthereumResourceId = createResourceID(sntEthereum.address, ETHEREUM_CHAIN_ID);
+    sntAvalancheResourceId = createResourceID(sntAvalanche.address, AVA_CHAIN_ID);
     const erc20HandlerFactory = await ethers.getContractFactory("ERC20Handler");
-    const _erc20Handler = await erc20HandlerFactory.deploy(
-      bridge.address,
+    const _ethereumErc20Handler = await erc20HandlerFactory.deploy(
+      EthereumBridge.address,
       [sntEthereumResourceId],
       [sntEthereum.address],
       []
     ) as ERC20Handler;
-    erc20Handler = _erc20Handler;
-    await bridge.adminSetResource(
-      erc20Handler.address,
+    ethereumErc20Handler = _ethereumErc20Handler;
+    await EthereumBridge.adminSetResource(
+      ethereumErc20Handler.address,
       sntEthereumResourceId,
       sntEthereum.address
     );
@@ -78,10 +93,10 @@ describe("Bridge", function () {
     recipientAddress = await recipient.getAddress();
     const sntDepositer = sntEthereum.connect(depositer);
     await sntEthereum.mint(depositerAddress, toWei("100"));
-    await sntDepositer.approve(erc20Handler.address, toWei("1"));
+    await sntDepositer.approve(ethereumErc20Handler.address, toWei("1"));
 
     encodedMetaData = createERCDepositData(toWei('1'), recipientAddress);
-    const bridgeDepositer = bridge.connect(depositer);
+    const bridgeDepositer = EthereumBridge.connect(depositer);
     const deposit = await bridgeDepositer.deposit(
       AVA_CHAIN_ID,
       sntEthereumResourceId,
@@ -96,9 +111,9 @@ describe("Bridge", function () {
 
   it("Should execute deposit", async function () {
     const [deployer, depositer, recipient, relayer1, relayer2] = accounts;
-    const bridgeRelayer1: Bridge = bridge.connect(relayer1);
-    const bridgeRelayer2: Bridge = bridge.connect(relayer2);
-    const depositDataHash = generateDepositDataHash(erc20Handler.address, encodedMetaData);
+    const bridgeRelayer1: Bridge = EthereumBridge.connect(relayer1);
+    const bridgeRelayer2: Bridge = EthereumBridge.connect(relayer2);
+    const depositDataHash = generateDepositDataHash(ethereumErc20Handler.address, encodedMetaData);
     const relay1Vote = await bridgeRelayer1.voteProposal(
       ETHEREUM_CHAIN_ID,
       depositNonce,
